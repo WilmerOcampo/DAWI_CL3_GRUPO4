fd72f106fe40801d1091a4aaea1d450e131202f5
Wilmer Ocampo Quispe <Wilmer_Ocampo@hotmail.com>
2024-05-10 21:16:37 -0500

    Creacion del componente Pregunta4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {Pregunta4Component} from "./pregunta4/pregunta4.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'pregunta4', component: Pregunta4Component},
+];
