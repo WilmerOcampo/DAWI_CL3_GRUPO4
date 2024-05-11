e8dec958d4c9f9379a5b251e1466095a3740f5bb
Natha23XO <hugocastroleon2301@gmail.com>
2024-05-10 21:59:30 -0500

    se culmina logica e implementacion de pregunta1

--- src/app/pregunta1/pregunta1.component.ts
+++ src/app/pregunta1/pregunta1.component.ts
@@ -13,12 +13,21 @@ export class Pregunta1Component {
   multiplicacion={
     precio : 0,
     cantidad : 0,
-    resultado:0
+    rcondescuento:0,
+    rsindescuento:0
   }
   mr = false
 
   calcularMultiplicacion(){
-    this.multiplicacion.resultado =Number(this.multiplicacion.cantidad) * Number(this.multiplicacion.precio)
+    this.multiplicacion.rsindescuento = 0;
+    this.multiplicacion.rcondescuento = 0;
+    var resultadomomentaneo = Number(this.multiplicacion.cantidad) * Number(this.multiplicacion.precio)
+    if(resultadomomentaneo > 200){
+      var descuento = resultadomomentaneo * 0.20
+      resultadomomentaneo -= descuento
+      this.multiplicacion.rcondescuento = resultadomomentaneo
+    }
+    this.multiplicacion.rsindescuento = Number(this.multiplicacion.cantidad) * Number(this.multiplicacion.precio)
     this.mr= true
   }
 
