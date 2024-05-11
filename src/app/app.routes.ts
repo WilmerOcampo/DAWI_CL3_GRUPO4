371dc82bbf1853845364ef3d8565003da3f68402
Genezi-00 <genesis@kaztro.cloud>
2024-05-10 22:53:45 -0500

    se elimina contenido app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { Pregunta3Component } from './pregunta3/pregunta3.component';
+
+export const routes: Routes = [{ path: 'pregunta3',component:Pregunta3Component}];
+
--- src/app/pregunta3/pregunta3.component.ts
+++ src/app/pregunta3/pregunta3.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-pregunta3',
   standalone: true,
-  imports: [],
+  imports: [
+    [CommonModule],
+    [FormsModule ]
+  ],
   templateUrl: './pregunta3.component.html',
   styleUrls: ['./pregunta3.component.scss'] 
 })
 export class Pregunta3Component {
   precioUnitario: number = 0; 
   cantidad: number = 0; 
-  precioTotal: number= 0;
+  precioTotal: number = 0;
 
   calcularPrecioTotal(): void {
     if (this.cantidad > 20) {
@@ -22,4 +27,3 @@ export class Pregunta3Component {
     }
   }
 }
-
