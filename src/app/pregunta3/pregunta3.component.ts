7bef7d439d0f1cf48b9a07d791d5595c90a5034a
Genezi-00 <genesis@kaztro.cloud>
2024-05-10 22:20:01 -0500

    Se configura angular form

--- src/app/pregunta3/pregunta3.component.ts
+++ src/app/pregunta3/pregunta3.component.ts
@@ -5,8 +5,21 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './pregunta3.component.html',
-  styleUrl: './pregunta3.component.scss'
+  styleUrls: ['./pregunta3.component.scss'] 
 })
 export class Pregunta3Component {
+  precioUnitario: number = 0; 
+  cantidad: number = 0; 
+  precioTotal: number= 0;
 
+  calcularPrecioTotal(): void {
+    if (this.cantidad > 20) {
+      this.precioTotal = this.precioUnitario * this.cantidad * 0.9; 
+    } else if (this.cantidad > 10) {
+      this.precioTotal = this.precioUnitario * this.cantidad * 0.95; 
+    } else {
+      this.precioTotal = this.precioUnitario * this.cantidad; 
+    }
+  }
 }
+
