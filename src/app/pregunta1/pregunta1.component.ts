b45e98ebf6aebe0ba149d1276cd3015688b8f3a2
Natha23XO <hugocastroleon2301@gmail.com>
2024-05-10 21:35:03 -0500

    se implementa logica de multiplicacion

--- src/app/pregunta1/pregunta1.component.ts
+++ src/app/pregunta1/pregunta1.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pregunta1',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './pregunta1.component.html',
   styleUrl: './pregunta1.component.scss'
 })
 export class Pregunta1Component {
+
+  multiplicacion={
+    precio : 0,
+    cantidad : 0,
+    resultado:0
+  }
+  mr = false
+
+  calcularMultiplicacion(){
+    this.multiplicacion.resultado =Number(this.multiplicacion.cantidad) * Number(this.multiplicacion.precio)
+    this.mr= true
+  }
 
 }
