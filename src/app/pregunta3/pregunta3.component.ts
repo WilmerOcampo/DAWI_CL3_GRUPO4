e7cb2c1c87e1ed95f04c0ae754ebd4617c063bcd
Wilmer Ocampo Quispe <Wilmer_Ocampo@hotmail.com>
2024-05-10 23:02:16 -0500

    Agregando navegacion a la pregunta 3

--- src/app/pregunta3/pregunta3.component.ts
+++ src/app/pregunta3/pregunta3.component.ts
@@ -1,29 +1,33 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
+import {RouterOutlet} from "@angular/router";
+import {NavegacionComponent} from "../navegacion/navegacion.component";
 
 @Component({
   selector: 'app-pregunta3',
   standalone: true,
   imports: [
     [CommonModule],
-    [FormsModule ]
+    [FormsModule],
+    RouterOutlet,
+    NavegacionComponent
   ],
   templateUrl: './pregunta3.component.html',
-  styleUrls: ['./pregunta3.component.scss'] 
+  styleUrls: ['./pregunta3.component.scss']
 })
 export class Pregunta3Component {
-  precioUnitario: number = 0; 
-  cantidad: number = 0; 
+  precioUnitario: number = 0;
+  cantidad: number = 0;
   precioTotal: number = 0;
 
   calcularPrecioTotal(): void {
     if (this.cantidad > 20) {
-      this.precioTotal = this.precioUnitario * this.cantidad * 0.9; 
+      this.precioTotal = this.precioUnitario * this.cantidad * 0.9;
     } else if (this.cantidad > 10) {
-      this.precioTotal = this.precioUnitario * this.cantidad * 0.95; 
+      this.precioTotal = this.precioUnitario * this.cantidad * 0.95;
     } else {
-      this.precioTotal = this.precioUnitario * this.cantidad; 
+      this.precioTotal = this.precioUnitario * this.cantidad;
     }
   }
 }
